ba1e0917007d0b3d80493e6ab5eef695fee88710
Samuel Soares <samuelj.soares@scranton.edu>
2015-12-10 12:42:35 -0500

    Layout updates.

---
+++ app/src/main/java/edu/scranton/cs/tictactoe/Scoreboard.java
@@ -0,0 +1,13 @@
+package edu.scranton.cs.tictactoe;
+
+/**
+ * Created by SamSo on 12/5/2015.
+ */
+public class Scoreboard {
+    private String usernames[];
+    private int wins[];
+    private int losses[];
+    private int draws[];
+    private int size;
+
+}
